2e9261c0be68c3e3e5c3a5cefa8606ad8c03676e
Aarsh <aarshstalati1989@gmail.com>
2017-01-14 16:22:03 -0500

    add code to read and update file attrib from file name

--- DateTimeAutoFix/Program.cs
+++ DateTimeAutoFix/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DateTimeAutoFix
 {
@@ -10,6 +9,39 @@ namespace DateTimeAutoFix
     {
         static void Main(string[] args)
         {
+            string strFileNameFormat = "yyyyMMdd_HHmmss";
+            string strDirectory = "D:\\test\\";
+            string fileName = null;
+
+            string[] files = Directory.GetFiles(strDirectory);
+            for (int i = 0, N = files.Count(); i < N; i++)
+            {
+                fileName = Path.GetFileNameWithoutExtension(files[i]);
+                DateTime fileModificationDateTime;
+
+                // Case 1: The file name is only date stamp as specified in format string
+                bool success = DateTime.TryParseExact(fileName, strFileNameFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out fileModificationDateTime);
+
+                if(!success)
+                {
+                    // Case 2: A valid file name ends with suffix. Workaround: remove suffix
+                    fileName = fileName.Substring(0, strFileNameFormat.Length);
+                    success = DateTime.TryParseExact(fileName, strFileNameFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out fileModificationDateTime);
+                }
+
+                if (success)
+                {
+                    Console.WriteLine("{0} \t\t\t {1}", fileName, fileModificationDateTime);
+                }
+                else
+                {
+                    Console.WriteLine("{0} - File format invalid", fileName);
+                }
+
+                File.SetCreationTime(files[i], fileModificationDateTime);
+                File.SetLastWriteTime(files[i], fileModificationDateTime);
+            }
+            Console.ReadKey();
         }
     }
 }
